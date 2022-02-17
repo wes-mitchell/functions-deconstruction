d0005d05e2a826d8e4e2969e76c15b4ff7de9081
wes-mitchell <wmdrums@gmail.com>
2022-02-17 11:15:44 -0600

    initial commit for this file

---
+++ main.js
@@ -0,0 +1,55 @@
+// Brush Your Teeth
+// 1. purchase toothbrush
+// 2. purchase toothpaste
+// 3. rinse toothbrush
+// 4. put toothpaste on toothbrush
+// 5. brush teeth
+// 5. rinse toothbrush
+// 6. put toothbrush away
+// 7. put toothpaste away
+
+const purchaseBrush = () => {
+    console.log(`Purchase toothbrush from grocery store.`)
+}
+
+const purchasePaste = () => {
+    console.log(`Purchase toothpaste from grocery store.`)
+}
+
+const rinseBrush = () => {
+    console.log(`Rinse toothbrush before use.`)
+}
+
+const pasteOnBrush = () => {
+    console.log(`Put toothpaste on toothbrush.`)
+}
+
+const brushTeeth = () => {
+    console.log(`Brush your teeth.`)
+}
+
+const finalRinseBrush = () => {
+    console.log(`Rinse your toothbrush`)
+}
+
+const putBrushAway = () => {
+    console.log(`Put toothbrush away.`)
+}
+
+const putPasteAway = () => {
+    console.log(`Put toothpaste away.`)
+}
+
+
+const brushYourTeeth = () => {
+purchaseBrush()
+purchasePaste()
+rinseBrush()
+pasteOnBrush()
+brushTeeth()
+finalRinseBrush()
+putBrushAway()
+putPasteAway()
+}
+
+brushYourTeeth()
